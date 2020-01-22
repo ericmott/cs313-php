61d8fc954a6ff936a0bdaa7ec88b3e55894db1bf
Eric Mott <mott.eric@sbcglobal.net>
2020-01-21 19:31:32 -0800

    new files

---
+++ web/ta03-php.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Assignment 03</title>
+</head>
+<body>
+    <div>
+        Username:
+        <?php
+        echo $_POST["name"]
+
+        ?>
+    </div>
+
+    <div>
+    Email:
+        <?php
+        echo $_POST["email"]
+        ?>
+    </div>
+
+    <div>
+    Major:
+        <?php
+        echo $_POST["major"]
+        ?>
+    </div>
+
+    <div>
+    Comments:
+        <?php
+        echo $_POST["comments"]
+        ?>
+    </div>
+
+    <div>
+        <?php
+        for ($i = 0; $i < 7; $i++) {
+            if (isset($_POST['continent'.$i])){
+                echo $_POST['coninent'.$i];
+            }
+        }
+        ?>
+    </div>
+
+
+
+
+
+</body>
+</html>
