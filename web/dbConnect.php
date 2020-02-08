41ff0a4c0acb3277923d3ff7dc933be57549b290
Eric Mott <mott.eric@sbcglobal.net>
2020-02-08 13:16:50 -0800

    Still trying

--- web/dbConnect.php
+++ web/dbConnect.php
@@ -2,28 +2,50 @@
 function get_db(){
     $db = NULL;
 
+// try
+// {
+//     $dbUrl = getenv('DATABASE_URL');
+
+//     $dbOpts = parse_url($dbUrl);
+
+//     $dbHost = $dbOpts["host"];
+//     $dbPort = $dbOpts["port"];
+//     $dbUser = $dbOpts["user"];
+//     $dbPassword = $dbOpts["pass"];
+//     $dbName = ltrim($dbOpts["path"],'/');
+
+//     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+//     $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION );
+// }
+// catch (PDOException $ex)
+// {
+//     echo 'Error!: ' . $ex-.getMessage();;
+//     die();
+// }
+// return $db;
+// }
+
 try
 {
-    $dbUrl = getenv('DATABASE_URL');
+  $dbUrl = getenv('DATABASE_URL');
 
-    $dbOpts = parse_url($dbUrl);
+  $dbOpts = parse_url($dbUrl);
 
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
 
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION );
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $ex)
 {
-    echo 'Error!: ' . $ex-.getMessage();;
-    die();
-}
-return $db;
+  echo 'Error!: ' . $ex->getMessage();
+  die();
 }
 
 ?>
