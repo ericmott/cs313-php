5c6e70878d8c68cf521fdf39dfff5e18c34f8853
Eric Mott <mott.eric@sbcglobal.net>
2020-02-08 13:25:16 -0800

    Test

--- web/ta05.php
+++ web/ta05.php
@@ -2,24 +2,24 @@
 
 try
 {
-    $dbUrl = getenv('DATABASE_URL');
+  $dbUrl = getenv('DATABASE_URL');
 
-    $dbOpts = parse_url($dbUrl);
+  $dbOpts = parse_url($dbUrl);
 
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"],'/');
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
 
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $ex)
 {
-    echo 'Error!: ' - $ex->getMessage();
-    die();
+  echo 'Error!: ' . $ex->getMessage();
+  die();
 }
 ?>
 
