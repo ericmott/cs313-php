4097c91d3f6e2557753925ac7a1ce0ff97d17d5b
Eric Mott <mott.eric@sbcglobal.net>
2020-03-01 21:34:10 -0800

    comment out new stuff

--- web/tracker-add2.php
+++ web/tracker-add2.php
@@ -4,62 +4,62 @@ require('dbConnect.php');
 $db = get_db();
 console.log('starting tracker-add2.php');
 
-// get the data from the POST
-$itemDescription = htmlspecialchars($_POST['description']);
-$model = htmlspecialchars($_POST['model']);
-$serialNumber = htmlspecialchars($_POST['serialNumber']);
-$purchasePrice = htmlspecialchars($_POST['purchasePrice']);
-$purchaseDate = htmlspecialchars($_POST['purchaseDate']);
-$storeName = htmlspecialchars($_POST['storeName']);  // *************************** Need to look up key if already existing ****
-$room = htmlspecialchars($_POST['room']);  // *************************** Need to look up key if already existing ****
-$firstName = htmlspecialchars($_POST['firstName']);  // *************************** Need to look up key if already existing ****
-$lastName = htmlspecialchars($_POST['lastName']);  // *************************** Need to look up key if already existing ****
+// // get the data from the POST
+// $itemDescription = htmlspecialchars($_POST['description']);
+// $model = htmlspecialchars($_POST['model']);
+// $serialNumber = htmlspecialchars($_POST['serialNumber']);
+// $purchasePrice = htmlspecialchars($_POST['purchasePrice']);
+// $purchaseDate = htmlspecialchars($_POST['purchaseDate']);
+// $storeName = htmlspecialchars($_POST['storeName']);  // *************************** Need to look up key if already existing ****
+// $room = htmlspecialchars($_POST['room']);  // *************************** Need to look up key if already existing ****
+// $firstName = htmlspecialchars($_POST['firstName']);  // *************************** Need to look up key if already existing ****
+// $lastName = htmlspecialchars($_POST['lastName']);  // *************************** Need to look up key if already existing ****
 
-// $store_id = 1;
-$room_id = 1;
-$owner_id = 1;
+// // $store_id = 1;
+// $room_id = 1;
+// $owner_id = 1;
 
-$storeQuery = 'SELECT storeName FROM store';
-$storeStmt = $db->prepare($storeQuery);
-$executeSuccess = $storeStmt->execute();
-$existingStoresInTable = $storeStmt->fetchAll(PDO::FETCH-ASSOC);
+// $storeQuery = 'SELECT storeName FROM store';
+// $storeStmt = $db->prepare($storeQuery);
+// $executeSuccess = $storeStmt->execute();
+// $existingStoresInTable = $storeStmt->fetchAll(PDO::FETCH-ASSOC);
 
-// If store table empty, add new store
-if (empty($existingStoresInTable)) {
-    console.log('In the empty IF statement');
-    // Add to DB
-    $query = 'INSERT INTO store(storeName) VALUES(:storeName)';
-    $statement = $db->prepare($query);
+// // If store table empty, add new store
+// if (empty($existingStoresInTable)) {
+//     console.log('In the empty IF statement');
+//     // Add to DB
+//     $query = 'INSERT INTO store(storeName) VALUES(:storeName)';
+//     $statement = $db->prepare($query);
     
-    $statement->bindValue(':storeName', $storeName);
-    $statement->execute();
+//     $statement->bindValue(':storeName', $storeName);
+//     $statement->execute();
 
-    $store_id = $db->lastInsertId(store_storeId_seq);
-} else {
-    console.log('in the not empty ELSE statement');
-    // check for existing store ID
-    $query = 'SELECT storeId, storeName FROM store WHERE storeName = :storeName';
-    $stmt = $db->prepare($query);
-    $stmt->execute();
-    $existingStore = $stmt->fetchAll(PDO::FETCH-ASSOC);
+//     $store_id = $db->lastInsertId(store_storeId_seq);
+// } else {
+//     console.log('in the not empty ELSE statement');
+//     // check for existing store ID
+//     $query = 'SELECT storeId, storeName FROM store WHERE storeName = :storeName';
+//     $stmt = $db->prepare($query);
+//     $stmt->execute();
+//     $existingStore = $stmt->fetchAll(PDO::FETCH-ASSOC);
 
-    if (empty($existingStore)) {
-        console.log('in the empty store IF statement');
-        // Add to DB
-        $query = 'INSERT INTO store(storeName) VALUES(:storeName)';
-        $statement = $db->prepare($query);
+//     if (empty($existingStore)) {
+//         console.log('in the empty store IF statement');
+//         // Add to DB
+//         $query = 'INSERT INTO store(storeName) VALUES(:storeName)';
+//         $statement = $db->prepare($query);
         
-        $statement->bindValue(':storeName', $storeName);
-        $statement->execute();
+//         $statement->bindValue(':storeName', $storeName);
+//         $statement->execute();
 
-        $store_id = $db->lastInsertId(store_storeId_seq);
-    } else {
-        console.log('in the empty store ELSE statement');
-        foreach ($existingStore as $store) {
-            $storeId = $store['storeId'];
-        }
-    }
-}
+//         $store_id = $db->lastInsertId(store_storeId_seq);
+//     } else {
+//         console.log('in the empty store ELSE statement');
+//         foreach ($existingStore as $store) {
+//             $storeId = $store['storeId'];
+//         }
+//     }
+// }
 
 console.log('out of the checking phase');
 
