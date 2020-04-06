e77147d392d950a247a682a4e5d9f9cbec8b5e40
Eric Mott <mott.eric@sbcglobal.net>
2020-04-05 20:23:12 -0700

    try

--- web/med-details.php
+++ web/med-details.php
@@ -9,10 +9,14 @@ require('dbConnect.php');
 $db = get_db();
 
 // Select all medication details
-$query = 'SELECT medId, medication, dosage, frequency, startDate, reason, medData_id, doc_id FROM medication WHERE medID = "$passMedId"'
+$query = "SELECT medId, medication, dosage, frequency, startDate, reason, medData_id, doc_id FROM medication WHERE medID = '" . $passMedId . "';";
 $stmt = $db->prepare($query);
 $stmt->execute();
 $med = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$a = trim($name); 
+$query = "SELECT surname FROM employee WHERE name= '" . $a . "';"; 
+$query = "SELECT surname FROM emploee WHERE name= '" . $name . "';";
 
 // $medId = $med['medId'];
 // $medication = $med['medication'];
