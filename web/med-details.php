de407ad58cb26cd9e758c9c3bd3fbb73e576ee5f
Eric Mott <mott.eric@sbcglobal.net>
2020-04-05 20:28:00 -0700

    try agaoin

--- web/med-details.php
+++ web/med-details.php
@@ -4,19 +4,20 @@ if (!isset($_GET['medId'])){
 }
 
 $passMedId = htmlspecialchars($_GET['medId']);
+$trimMedId = trim($passMedId);
 
 require('dbConnect.php');
 $db = get_db();
 
 // Select all medication details
-$query = "SELECT medId, medication, dosage, frequency, startDate, reason, medData_id, doc_id FROM medication WHERE medID = '" . $passMedId . "'";
+$query = "SELECT medId, medication, dosage, frequency, startDate, reason, medData_id, doc_id FROM medication WHERE medID = '" . $passMedId . "';";
 $stmt = $db->prepare($query);
 $stmt->execute();
 $med = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$a = trim($name); 
-$query = "SELECT surname FROM employee WHERE name= '" . $a . "';"; 
-$query = "SELECT surname FROM emploee WHERE name= '" . $name . "';";
+// $a = trim($name); 
+// $query = "SELECT surname FROM employee WHERE name= '" . $a . "';"; 
+// $query = "SELECT surname FROM emploee WHERE name= '" . $name . "';";
 
 // $medId = $med['medId'];
 // $medication = $med['medication'];
