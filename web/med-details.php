32fb0792d8b4032a564ee408b1a4c7e4e60945a0
Eric Mott <mott.eric@sbcglobal.net>
2020-04-05 18:53:28 -0700

    add Details and link

---
+++ web/med-details.php
@@ -0,0 +1,39 @@
+<?php
+if (!isset($_GET['medId'])){
+    die("Error, med ID not set");
+}
+
+$passMedId = htmlspecialchars($_GET['medId']);
+
+require('dbConnect.php');
+$db = get_db();
+
+// Select all medication details
+$query = 'SELECT medId, medication, dosage, frequency, startDate, reason, medData_id, doc_id FROM medication WHERE medID = $passMedId';
+$stmt = $db->prepare($query);
+$stmt->execute();
+$med = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$medId = $med['medId'];
+$medication = $med['medication'];
+$dosage = $med['dosage'];
+$frequency = $med['frequency'];
+$startDate = $med['startDate'];
+$reason = $med['reason'];
+$medData_id = $med['medData_id'];
+$doc_id = $med['doc_id'];
+
+var_dump($med);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medication Details</title>
+</head>
+<body>
+    
+</body>
+</html>
