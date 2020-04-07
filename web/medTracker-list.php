abaff7f77da21acbc6d302bdd266c72400ac7511
Eric Mott <mott.eric@sbcglobal.net>
2020-04-06 19:58:26 -0700

    change order

--- web/medTracker-list.php
+++ web/medTracker-list.php
@@ -3,7 +3,7 @@ require('dbConnect.php');
 $db = get_db();
 
 // Select medID, medication, dosage, frequency, reason, medData_id, doc_id FROM medication
-$query = 'SELECT medId, medication, dosage, frequency, reason, meddata_id, doc_id, doclastname, docid FROM medication
+$query = 'SELECT medId, medication, dosage, frequency, reason, meddata_id, doc_id, docid, doclastname FROM medication
  LEFT OUTER JOIN doctor ON (medication.doc_id = doctor.docid)';
 $stmt = $db->prepare($query);
 $stmt->execute();
@@ -45,8 +45,8 @@ $meds = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 $reason = $med['reason'];
                 $medData_id = $med['meddata_id'];
                 $doc_id = $med['doc_id'];
-                $docLastName = $med['doclastname'];
                 $docId - $med['docid'];
+                $docLastName = $med['doclastname'];
 
                 echo "<tr><td><a href='med-details.php?medId=$medId'>$medication</a></td><td>$dosage</td><td>$frequency</td><td>$reason</td><td><a href='med-docDetails.php?docId=$docId'>Dr. $docLastName</a></td><td>medId: $medId</td><td>docId: $docId</td><td>doc_id: $doc_id</td></tr>";
                 var_dump($medID);
