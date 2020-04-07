5575116f187dea9df320251b0c9b28d8219f59d8
Eric Mott <mott.eric@sbcglobal.net>
2020-04-06 22:49:53 -0700

    add $

--- web/med-updateMed.php
+++ web/med-updateMed.php
@@ -1,12 +1,12 @@
 <?php
 
-$medId = htmlspecialchars(_POST['medId']);
-$medication = htmlspecialchars(_POST['medication']);
-$dosage = htmlspecialchars(_POST['dosage']);
-$frequency = htmlspecialchars(_POST['frequency']);
-$startDate = htmlspecialchars(_POST['startDate']);
-$endDate = htmlspecialchars(_POST['endDate']);
-$reason = htmlspecialchars(_POST['reason']);
+$medId = htmlspecialchars($_POST['medId']);
+$medication = htmlspecialchars($_POST['medication']);
+$dosage = htmlspecialchars($_POST['dosage']);
+$frequency = htmlspecialchars($_POST['frequency']);
+$startDate = htmlspecialchars($_POST['startDate']);
+$endDate = htmlspecialchars($_POST['endDate']);
+$reason = htmlspecialchars($_POST['reason']);
 
 echo "$medication\n";
 echo $reason;
