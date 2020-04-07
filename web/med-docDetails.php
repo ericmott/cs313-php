80e32c0874eca5282739c733709fa9fc23bef88a
Eric Mott <mott.eric@sbcglobal.net>
2020-04-06 18:42:39 -0700

    fix table name

--- web/med-docDetails.php
+++ web/med-docDetails.php
@@ -10,7 +10,7 @@ require('dbConnect.php');
 $db = get_db();
 
 // Select all medication details
-$query = "SELECT docid, docfirstname, doclastname, specialty, address_1, address_2, city, stateAbrev, zip, phone FROM doctor WHERE medID = $passDocId";
+$query = 'SELECT docid, docfirstname, doclastname, specialty, address_1, address_2, city, stateAbrev, zip, phone FROM doctor';
 $stmt = $db->prepare($query);
 $stmt->execute();
 $doc = $stmt->fetchAll(PDO::FETCH_ASSOC);
