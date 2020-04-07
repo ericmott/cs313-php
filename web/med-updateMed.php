1bc61787b3027acf08590a97c3885c190dd5370d
Eric Mott <mott.eric@sbcglobal.net>
2020-04-07 16:49:53 -0700

    check passed variables

--- web/med-updateMed.php
+++ web/med-updateMed.php
@@ -8,8 +8,12 @@ $startDate = htmlspecialchars($_POST['startDate']);
 $endDate = htmlspecialchars($_POST['endDate']);
 $reason = htmlspecialchars($_POST['reason']);
 
+echo "$medId\n";
 echo "$medication\n";
+echo "$dosage\n";
+echo "$frequency\n";
+echo "$startDate\n";
+echo "$endDate\n";
 echo $reason;
 
-echo var_dump($medication);
 ?>
