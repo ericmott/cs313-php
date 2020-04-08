97bf161628eb00cfaeb6ca3f6a1d44fff46e22d6
Eric Mott <mott.eric@sbcglobal.net>
2020-04-07 22:10:28 -0700

    echo medID

--- web/med-deleteMed.php
+++ web/med-deleteMed.php
@@ -6,22 +6,22 @@ if(!isset($_GET['medId'])){
 
 $medId = htmlspecialchars($_GET['medId']);
 
-// echo "$medId\n";
-// echo $medId;
+echo "$medId\n";
+echo $medId;
 
 
-require('dbConnect.php');
-$db = get_db();
+// require('dbConnect.php');
+// $db = get_db();
 
-// Delete medication from table
-$deleteData = 'DELETE FROM medication WHERE medid = :medid';
-$stmt = $db->prepare($deleteData);
-$stmt->bindValue(':medid', $medId, PDO::PARAM_INT);
-$stmt->execute();
+// // Delete medication from table
+// $deleteData = 'DELETE FROM medication WHERE medid = :medid';
+// $stmt = $db->prepare($deleteData);
+// $stmt->bindValue(':medid', $medId, PDO::PARAM_INT);
+// $stmt->execute();
 
-$new_page = "medTracker-list.php";
+// $new_page = "medTracker-list.php";
 
-header("Location: $new_page");
-die();
+// header("Location: $new_page");
+// die();
 
 ?>
