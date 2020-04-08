b534e45d1e3c7ae01b75fb1c5b626966e9d1e358
Eric Mott <mott.eric@sbcglobal.net>
2020-04-07 18:23:39 -0700

    experimenting

--- web/med-addNewMed.php
+++ web/med-addNewMed.php
@@ -23,7 +23,7 @@ $db = get_db();
 
 // Add new medication to table          **** Need to link medData_id and doc_id ***
 $addMed = 'INSERT INTO medication (medication, dosage, frequency, startDate, endDate, reason, medData_id, doc_id) VALUES (:medication, :dosage, :frequency, :startDate, :endDate, :reason, :medData_id, :doc_id)';
-$stmt = $db->prepare($addMed);
+$stmt = $db->prepare("$addMed");
 // $stmt = $db->prepare('INSERT INTO medication (medication, dosage, frequency, startDate, endDate, reason, medData_id, doc_id) VALUES (:medication, :dosage, :frequency, :startDate, :endDate, :reason, :medData_id, :doc_id)');
 $stmt->bindValue(':medication', $medication, PDO::PARAM_STR);
 $stmt->bindValue(':dosage', $dosage, PDO::PARAM_STR);
