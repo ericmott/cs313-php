41135e46e94cd95ff7c7c95a786465fbed6e1c47
Eric Mott <mott.eric@sbcglobal.net>
2020-04-07 19:47:08 -0700

    add UPDATE

--- web/med-updateMed.php
+++ web/med-updateMed.php
@@ -16,4 +16,25 @@ echo "$startDate\n";
 echo "$endDate\n";
 echo $reason;
 
+require('dbConnect.php');
+$db = get_db();
+
+// Add new medication to table          **** Need to link medData_id and doc_id ***
+$addData = 'UPDATE medication SET medication = :medication, dosage = :dosage, frequency = :frequency, startDate = :startDate, endDate = :endDate, reason = :reason, medData_id = :medData_id, doc_id = :doc_id WHERE medid = :medid';
+$stmt = $db->prepare($addData);
+$stmt->bindValue(':medication', $medication, PDO::PARAM_STR);
+$stmt->bindValue(':dosage', $dosage, PDO::PARAM_STR);
+$stmt->bindValue(':frequency', $frequency, PDO::PARAM_STR);
+$stmt->bindValue(':startDate', $startDate, PDO::PARAM_STR);
+$stmt->bindValue(':endDate', $endDate, PDO::PARAM_STR);
+$stmt->bindValue(':reason', $reason, PDO::PARAM_STR);
+$stmt->bindValue(':medData_id', $medData_id, PDO::PARAM_INT);
+$stmt->bindValue(':doc_id', $doc_id, PDO::PARAM_INT);
+$stmt->bindValue(':medid', $medId, PDO::PARAM_INT);
+$stmt->execute();
+
+$new_page = "medTracker-list.php";
+
+header("Location: $new_page");
+die();
 ?>
