31175d8df1a56e2e55ad2be43c8b8b5b7eb2873e
Eric Mott <mott.eric@sbcglobal.net>
2020-04-07 21:59:03 -0700

    delete Med

--- web/med-deleteMed.php
+++ web/med-deleteMed.php
@@ -6,32 +6,22 @@ if(!isset($_GET['medId'])){
 
 $medId = htmlspecialchars($_GET['medId']);
 
+// echo "$medId\n";
+// echo $medId;
 
 
+require('dbConnect.php');
+$db = get_db();
 
-echo "$medId\n";
-echo $medId;
+// Delete medication from table
+$deleteData = 'DELETE FROM medication WHERE medId = :medId';
+$stmt = $db->prepare($deleteData);
+$stmt->bindValue(':medId', $medId, PDO::PARAM_INT);
+$stmt->execute();
 
+$new_page = "medTracker-list.php";
 
-// require('dbConnect.php');
-// $db = get_db();
+header("Location: $new_page");
+die();
 
-// // Add new medication to table          **** Need to link medData_id and doc_id ***
-// $addData = 'INSERT INTO medication (medication, dosage, frequency, startDate, endDate, reason, medData_id, doc_id) VALUES (:medication, :dosage, :frequency, :startDate, :endDate, :reason, :medData_id, :doc_id)';
-// $stmt = $db->prepare($addData);
-// // $stmt = $db->prepare('INSERT INTO medication (medication, dosage, frequency, startDate, endDate, reason, medData_id, doc_id) VALUES (:medication, :dosage, :frequency, :startDate, :endDate, :reason, :medData_id, :doc_id)');
-// $stmt->bindValue(':medication', $medication, PDO::PARAM_STR);
-// $stmt->bindValue(':dosage', $dosage, PDO::PARAM_STR);
-// $stmt->bindValue(':frequency', $frequency, PDO::PARAM_STR);
-// $stmt->bindValue(':startDate', $startDate, PDO::PARAM_STR);
-// $stmt->bindValue(':endDate', $endDate, PDO::PARAM_STR);
-// $stmt->bindValue(':reason', $reason, PDO::PARAM_STR);
-// $stmt->bindValue(':medData_id', $medData_id, PDO::PARAM_INT);
-// $stmt->bindValue(':doc_id', $doc_id, PDO::PARAM_INT);
-// $stmt->execute();
-
-// $new_page = "medTracker-list.php";
-
-// header("Location: $new_page");
-// die();
 ?>
