755a26dbb9c9c0226cdb83143696dcf3c76e3839
Eric Mott <mott.eric@sbcglobal.net>
2020-04-07 18:07:28 -0700

    change camelCase

--- web/med-addNewMed.php
+++ web/med-addNewMed.php
@@ -24,14 +24,14 @@ $db = get_db();
 // Add new medication to table          **** Need to link medData_id and doc_id ***
 // $addMed = 'INSERT INTO medication (medication, dosage, frequency, startDate, endDate, reason, medData_id, doc_id) VALUES (:medication, :dosage, :frequency, :startDate, :endDate, :reason, :medData_id, doc_id)';
 // $stmt = $db->prepare($addMed);
-$stmt = $db->prepare('INSERT INTO medication (medication, dosage, frequency, startdate, enddate, reason, meddata_id, doc_id) VALUES (:medication, :dosage, :frequency, :startDate, :endDate, :reason, :medData_id, doc_id)');
+$stmt = $db->prepare('INSERT INTO medication (medication, dosage, frequency, startdate, enddate, reason, meddata_id, doc_id) VALUES (:medication, :dosage, :frequency, :startdate, :enddate, :reason, :meddata_id, doc_id)');
 $stmt->bindValue(':medication', $medication, PDO::PARAM_STR);
 $stmt->bindValue(':dosage', $dosage, PDO::PARAM_STR);
 $stmt->bindValue(':frequency', $frequency, PDO::PARAM_STR);
-$stmt->bindValue(':startDate', $startDate, PDO::PARAM_STR);
-$stmt->bindValue(':endDate', $endDate, PDO::PARAM_STR);
+$stmt->bindValue(':startdate', $startDate, PDO::PARAM_STR);
+$stmt->bindValue(':enddate', $endDate, PDO::PARAM_STR);
 $stmt->bindValue(':reason', $reason, PDO::PARAM_STR);
-$stmt->bindValue(':medData_id', $medData_id, PDO::PARAM_INT);
+$stmt->bindValue(':meddata_id', $medData_id, PDO::PARAM_INT);
 $stmt->bindValue(':doc_id', $doc_id, PDO::PARAM_INT);
 $stmt->execute();
 
